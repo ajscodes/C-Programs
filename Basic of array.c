7dcd9ac8ce23c5f37a2433e6299ac4535db278f6
ajscodes <ayushmaradia@gmail.com>
2024-06-21 22:47:35 +0530

    Array Info

---
+++ Basic of array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int a[3][3],i,j,pos,neg,zero;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+		printf("\n");
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+}
